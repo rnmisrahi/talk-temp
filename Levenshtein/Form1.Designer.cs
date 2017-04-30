352d62a56f3a0f7c4442ec32d44097c02596b11e
rnmisrahi <rnmisrahi@gmail.com>
2017-04-30 15:12:19 +0300

    Add project files.

---
+++ Levenshtein/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Levenshtein
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.panel1 = new System.Windows.Forms.Panel();
+      this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.textBox2 = new System.Windows.Forms.TextBox();
+      this.btnCompare = new System.Windows.Forms.Button();
+      this.label1 = new System.Windows.Forms.Label();
+      this.panel1.SuspendLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+      this.splitContainer1.Panel1.SuspendLayout();
+      this.splitContainer1.Panel2.SuspendLayout();
+      this.splitContainer1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // panel1
+      // 
+      this.panel1.Controls.Add(this.label1);
+      this.panel1.Controls.Add(this.btnCompare);
+      this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+      this.panel1.Location = new System.Drawing.Point(0, 390);
+      this.panel1.Name = "panel1";
+      this.panel1.Size = new System.Drawing.Size(822, 89);
+      this.panel1.TabIndex = 0;
+      // 
+      // splitContainer1
+      // 
+      this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+      this.splitContainer1.Name = "splitContainer1";
+      // 
+      // splitContainer1.Panel1
+      // 
+      this.splitContainer1.Panel1.Controls.Add(this.textBox1);
+      // 
+      // splitContainer1.Panel2
+      // 
+      this.splitContainer1.Panel2.Controls.Add(this.textBox2);
+      this.splitContainer1.Size = new System.Drawing.Size(822, 390);
+      this.splitContainer1.SplitterDistance = 412;
+      this.splitContainer1.TabIndex = 1;
+      // 
+      // textBox1
+      // 
+      this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.textBox1.Location = new System.Drawing.Point(0, 0);
+      this.textBox1.Multiline = true;
+      this.textBox1.Name = "textBox1";
+      this.textBox1.Size = new System.Drawing.Size(412, 390);
+      this.textBox1.TabIndex = 0;
+      // 
+      // textBox2
+      // 
+      this.textBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.textBox2.Location = new System.Drawing.Point(0, 0);
+      this.textBox2.Multiline = true;
+      this.textBox2.Name = "textBox2";
+      this.textBox2.Size = new System.Drawing.Size(406, 390);
+      this.textBox2.TabIndex = 0;
+      // 
+      // btnCompare
+      // 
+      this.btnCompare.Location = new System.Drawing.Point(671, 28);
+      this.btnCompare.Name = "btnCompare";
+      this.btnCompare.Size = new System.Drawing.Size(75, 26);
+      this.btnCompare.TabIndex = 0;
+      this.btnCompare.Text = "Compare";
+      this.btnCompare.UseVisualStyleBackColor = true;
+      this.btnCompare.Click += new System.EventHandler(this.btnCompare_Click);
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(25, 36);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(46, 17);
+      this.label1.TabIndex = 1;
+      this.label1.Text = "label1";
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(822, 479);
+      this.Controls.Add(this.splitContainer1);
+      this.Controls.Add(this.panel1);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.panel1.ResumeLayout(false);
+      this.panel1.PerformLayout();
+      this.splitContainer1.Panel1.ResumeLayout(false);
+      this.splitContainer1.Panel1.PerformLayout();
+      this.splitContainer1.Panel2.ResumeLayout(false);
+      this.splitContainer1.Panel2.PerformLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+      this.splitContainer1.ResumeLayout(false);
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Panel panel1;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Button btnCompare;
+    private System.Windows.Forms.SplitContainer splitContainer1;
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.TextBox textBox2;
+  }
+}
+
